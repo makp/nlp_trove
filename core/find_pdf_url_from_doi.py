910861335f276fdef06fc492791c096d02728d6c
mak-leibniz <makmiller@gmail.com>
2023-08-27 19:32:19 -0400

    Rename funcs and vars

--- core/find_pdf_url_from_doi.py
+++ core/find_pdf_url_from_doi.py
@@ -1,9 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-# import logging
 
 
-def extract_pdf_url_from_doi_html(html_content, tag, attr_dict):
+def extract_content_from_html(html_content, tag, attr_dict):
     soup = BeautifulSoup(html_content, 'html.parser')
     meta_tag = soup.find(tag, attr_dict)
     if meta_tag:
@@ -12,14 +11,14 @@ def extract_pdf_url_from_doi_html(html_content, tag, attr_dict):
         return None
 
 
-def get_pdf_url_from_doi_url(doi_url, tag, attr_dict):
+def extract_content_from_url(url, tag, attr_dict):
     try:
-        response = requests.get(doi_url)
+        response = requests.get(url)
         response.raise_for_status()
     except (requests.RequestException, ValueError):
-        print(f"Invalid DOI or request failed for DOI URL: {doi_url}")
-        with open("failed_doi_requests.txt", "a") as f:
-            f.write(doi_url + "\n")
+        print(f"Invalid URL: {url}")
+        with open("failed_requests.txt", "a") as f:
+            f.write(url + "\n")
         return
     page_content = response.text
-    return extract_pdf_url_from_doi_html(page_content, tag, attr_dict)
+    return extract_content_from_html(page_content, tag, attr_dict)
