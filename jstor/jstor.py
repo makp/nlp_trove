899d1a2aa9a79e4520b93bfdbcae13e935cfcc84
mak-turing <makmiller@gmail.com>
2023-08-31 15:52:14 -0400

    Add func to check if JSTOR articles are in the main df

--- jstor/jstor.py
+++ jstor/jstor.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def extract_jstor_id_from_jstor_url(jstor_url):
     return jstor_url.split('/')[-1]
 
@@ -7,3 +10,24 @@ def generate_jstor_link_to_pdf(jstor_url):
     url_base = 'www.jstor.org/stable/pdf/'
     jstor_id = extract_jstor_id_from_jstor_url(jstor_url)
     return f"{url_base}{jstor_id}.pdf"
+
+
+def are_jstor_articles_in_main_df(df, df_jstor,
+                                  columns=['title', 'title']):
+    """
+    Check if all articles in df_jstor are present in df based on a
+    specified column.
+    """
+    if columns[0] not in df.columns or columns[1] not in df_jstor.columns:
+        return "Invalid column names"
+
+    merged_df = pd.merge(df, df_jstor,
+                         left_on=columns[0],
+                         right_on=columns[1],
+                         how='inner')
+    if len(merged_df) == len(df_jstor):
+        return True
+    else:
+        missing_articles = len(df_jstor) - len(merged_df)
+        print(f"{missing_articles} articles not present in the main DataFrame")
+        return False
