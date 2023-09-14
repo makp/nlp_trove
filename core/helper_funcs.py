1a73b7b9749c1cca7f1c294ae7f021d46a6958b8
mak-turing <makmiller@gmail.com>
2023-09-14 10:40:44 -0400

    Add func to incorporate individual pdf files to database

--- core/helper_funcs.py
+++ core/helper_funcs.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import hashlib
 from datetime import datetime
 
 
@@ -26,6 +27,22 @@ def is_pdf(filepath):
     with open(filepath, 'rb') as f:
         header = f.read(5)
     return header == b"%PDF-"
+
+
+def incorporate_pdf_into_database(pdf_path, database_dir, df, row_indx, column_name='pdf_filename'):
+    """
+    Rename the PDF using MD5 hash, save it to the database directory, and
+    update the corresponding row in DataFrame.
+    """
+    with open(pdf_path, 'rb') as f:
+        file_content = f.read()
+        md5_hash = hashlib.md5(file_content).hexdigest()
+
+    new_name = f"{md5_hash}.pdf"
+    os.rename(pdf_path, os.path.join(database_dir, new_name))
+
+    df.at[row_indx, column_name] = new_name
+    return df.loc[row_indx]
 
 
 def calculate_date_range(df, column_name='publicationDate'):
