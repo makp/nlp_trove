fde777d996963ac0061fa47ddec01784a51de45b
mak-turing <makmiller@gmail.com>
2023-09-20 00:33:44 -0400

    Accept int as argument

--- core/helper_funcs.py
+++ core/helper_funcs.py
@@ -49,12 +49,16 @@ def incorporate_pdf_into_database(pdf_path, database_dir, df, row_indx, column_n
 
 def del_rows_and_corresponding_pdfs(idx, df, pdf_folder, pdf_column='pdf_filename'):
     """Delete rows from a DataFrame and the corresponding PDF files."""
+    if isinstance(idx, int):
+        idx = [idx]
     pdf_filenames = df.loc[idx, pdf_column]
     pdf_paths = [os.path.join(pdf_folder, f) for f in pdf_filenames]
     df.drop(idx, inplace=True)
+    print(f"Rows {idx} deleted from DataFrame.")
     for path in pdf_paths:
         if os.path.isfile(path):
             os.remove(path)
+            print(f"File {path} deleted.")
         else:
             print(f"File {path} does not exist. Skipping deletion.")
 
