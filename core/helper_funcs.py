ecd6fd884449962c4fe6cfd80268468cd4f6df4d
mak-turing <makmiller@gmail.com>
2023-09-21 14:20:49 -0400

    Add helper func for listing database files

--- core/helper_funcs.py
+++ core/helper_funcs.py
@@ -1,5 +1,4 @@
 import os
-import pandas as pd
 import hashlib
 from datetime import datetime
 import requests
@@ -22,6 +21,20 @@ def save_df(df, folder_path, prefix='out', suffix='', extension='pkl'):
     method_name, kwargs = method_map[extension]
     getattr(df, method_name)(full_path, kwargs)
     return print(f"File: {full_path};\nData shape: {df.shape}")
+
+
+def find_database_file(database_dir, filter_str=None, extension='pkl'):
+    files = [f for f in os.listdir(database_dir) if f.endswith(extension)]
+    if filter_str:
+        files = [f for f in files if filter_str in f]
+
+    if not files:
+        return None
+
+    print(f"Found {len(files)} files")
+
+    files = [os.path.join(database_dir, f) for f in files]
+    return sorted(files, key=os.path.getctime)
 
 
 def is_pdf(filepath):
