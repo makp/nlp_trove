3d11c1c3961ea57c73fbe041d0ea0ca2b9d18570
mak-turing <makmiller@gmail.com>
2023-11-10 21:52:21 -0500

    Group cleaning routines into a class

--- preprocessing/clean_text.py
+++ preprocessing/clean_text.py
@@ -1,75 +1,59 @@
-"""Remove certain characters from text."""
+"""Clean text."""
 
-import re
 import html
 import textacy.preprocessing as tp
-# from functools import partial
 from bs4 import BeautifulSoup, Comment
 
 
-def clean_html(text):
-    """Clean HTML text."""
-    text = html.unescape(text)  # convert html escape to characters
+class TextCleaner:
+    """Class for cleaning text."""
 
-    # parse HTML
-    soup = BeautifulSoup(text, 'lxml')
+    def __init__(self):
+        """Initialize the TextCleaner class."""
+        self.normalize_text = tp.make_pipeline(
+            tp.normalize.bullet_points,
+            tp.normalize.hyphenated_words,  # reattach separated by line breaks
+            tp.normalize.quotation_marks,
+            tp.normalize.unicode)
 
-    # remove certain tags
-    for tag in soup(["script", "style"]):
-        tag.decompose()
+        self.replace_from_text = tp.make_pipeline(
+            tp.replace.urls,
+            tp.replace.emails,
+            tp.replace.phone_numbers,
+            tp.replace.numbers,
+            tp.replace.currency_symbols)
 
-    # remove comments
-    for comment in soup.find_all(string=lambda t: isinstance(t, Comment)):
-        comment.extract()    # comment doesn't have decompose() method
+        self.remove_from_text = tp.make_pipeline(
+            tp.remove.accents,
+            tp.remove.punctuation)
 
-    # get untagged text
-    text = soup.get_text()
+        def clean_html(self, text):
+            """Clean HTML text."""
+            text = html.unescape(text)  # convert html escape to characters
 
-    return text
+            # parse HTML
+            soup = BeautifulSoup(text, 'lxml')
 
+            # remove certain tags
+            for tag in soup(["script", "style"]):
+                tag.decompose()
 
-normalize_text = tp.make_pipeline(
-    tp.normalize.bullet_points,
-    tp.normalize.hyphenated_words,  # reattach separated by line breaks
-    tp.normalize.quotation_marks,
-    tp.normalize.unicode)
+            # remove comments
+            for comment in soup.find_all(string=lambda t:
+                                         isinstance(t, Comment)):
+                comment.extract()  # comment doesn't have decompose() method
 
+            # get untagged text
+            text = soup.get_text()
 
-replace_from_text = tp.make_pipeline(
-    tp.replace.urls,
-    tp.replace.emails,
-    tp.replace.phone_numbers,
-    tp.replace.numbers,
-    tp.replace.currency_symbols)
+            return text
 
-
-remove_from_text = tp.make_pipeline(
-    tp.remove.accents,
-    tp.remove.punctuation)
-
-
-def clean_text(text, is_html=False):
-    """Clean text."""
-    if is_html:
-        text = clean_html(text)
-    text = normalize_text(text)
-    text = replace_from_text(text)
-    text = remove_from_text(text)
-    text = tp.normalize.whitespace(text)
-    return text
-
-
-# # remove special characters
-# text = re.sub(r'[^A-Za-z0-9\s]+', '', text)
-
-
-# Section
-# -------
-# Calculate the degree of "impurity" in a text
-
-RE_SUSPICIOUS = re.compile(r'[&#<>{}\[\]\\]')
-
-
-def calc_degree_impurity(text):
-    """Calculate the degree of impurity in a text."""
-    return len(RE_SUSPICIOUS.findall(text)/len(text))
+        def clean_text(self, text, is_html=False):
+            """Clean text."""
+            if is_html:
+                text = clean_html(text)
+            text = self.normalize_text(text)
+            text = self.replace_from_text(text)
+            text = self.remove_from_text(text)
+            text = tp.normalize.whitespace(text)
+            return text
