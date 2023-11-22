cb00fc88487f0c8c4aa67b57919c89491c11a44c
mak-turing <makmiller@gmail.com>
2023-11-22 10:18:40 -0500

    Add routine for running dbscan

--- models/cluster.py
+++ models/cluster.py
@@ -1,7 +1,8 @@
 """Functions for clustering and clustering evaluation."""
 
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, DBSCAN
 from sklearn.metrics import silhouette_score
+from collections import Counter
 import numpy as np
 
 
@@ -22,3 +23,28 @@ def evaluate_kmeans_clusters(vecs, n_clusters):
     var = np.var(cluster_sizes)
 
     return [sil_score, var]
+
+
+def apply_dbscan(vecs, eps=0.5, min_samples=5):
+    """
+    Apply DBSCAN clustering to the vectors.
+
+    Notes about the parameters:
+    - The `eps` parameter defines the radius of neighborhood around a
+      point. A small `eps` can divide the data in many small clusters.
+    - The `min_samples` parameter reflects the minimum number of
+      samples in a nb for a point to be considered a core point,
+      including the point itself. A larger `min_samples` will result
+      in a greater density to form a cluster.
+    """
+    # Generate DBSCAN clusters
+    dbscan = DBSCAN(eps=eps, min_samples=min_samples)
+    clusters = dbscan.fit_predict(vecs)
+
+    # Count the number of vectors in each cluster
+    cluster_counts = Counter(clusters)
+
+    # Find indices of outliers
+    noise_indices = np.where(clusters == -1)[0]
+
+    return clusters, cluster_counts, noise_indices
