bff2e5ff5348c014f16a927239fbb8d92a9e29d5
mak-turing <makmiller@gmail.com>
2023-11-26 15:38:33 -0500

    Add func for generating TF-IDF vecs; Rename folder

--- vectorization/sparse_vectorization.py
+++ vectorization/sparse_vectorization.py
@@ -1,6 +1,7 @@
 """Transform text into sparse vectors."""
 
 from gensim.corpora import Dictionary
+from gensim.models import TfidfModel
 
 
 def map_tokens_to_integer_ids(tokenized_docs,
@@ -20,3 +21,15 @@ def map_tokens_to_integer_ids(tokenized_docs,
                             no_above=max_doc_frac)
 
     return id2word
+
+
+def build_tfidf_vectors(tokenized_docs, id2word):
+    """Build TF-IDF vectors from tokenized documents."""
+    # Convert tokenized documents into bow vectors
+    bow_corpus = [id2word.doc2bow(doc) for doc in tokenized_docs]
+
+    # Fit the TF-IDF model
+    tfidf = TfidfModel(dictionary=id2word)
+
+    # Return corpus as TF-IDF vectors
+    return tfidf[bow_corpus]
