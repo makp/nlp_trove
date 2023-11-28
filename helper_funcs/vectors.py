ee9ca8d47b9aa05eb72afbf9fa06371fba6c3ced
mak-turing <makmiller@gmail.com>
2023-11-28 00:24:06 -0500

    Merge files

--- helper_funcs/vectors.py
+++ helper_funcs/vectors.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 from gensim import matutils
+from gensim.matutils import cossim
+from gensim.matutils import kullback_leibler
 import pickle
 
 
@@ -50,3 +52,25 @@ def load_sbert_embeddings(fname='sbert_embeddings.pkl'):
     with open(fname, "rb") as fIn:
         data = pickle.load(fIn)
     return data['sentences'], data['embeddings']
+
+
+def get_cosine_distance(vec1, vec2):
+    """Compute the cosine distance between two vectors."""
+    return 1 - cossim(vec1, vec2)
+
+
+def calculate_jensen_shannon_div(vec1, vec2):
+    """
+    Calculate the Jensen-Shannon divergence between two vectors.
+
+    The Jensen-Shannon Divergence (JS) is the average of two
+    Kullback-Leibler Divergences (KL). But unlike KL, JS is symmetric and
+    bounded between 0 and 1. JS represents a type of average
+    'information gain' of one distribution over the other.
+    """
+    # Calculate the average of the two vectors
+    m = 0.5 * (vec1 + vec2)
+
+    # Calculate Jensen-Shannon divergence
+    return 0.5 * (kullback_leibler(vec1, m) +
+                  kullback_leibler(vec2, m))
