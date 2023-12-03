44d93a36e3ab9d8283956c49942ae2db509bea06
mak-turing <makmiller@gmail.com>
2023-12-03 00:56:42 -0500

    Implement different summarization algorithms with summy

--- models/summarization.py
+++ models/summarization.py
@@ -1,83 +1,81 @@
 """
 Text summarization.
 
+Algorithms:
+- LSA
+- TextRank
+- TF-IDF
+
 Notes:
 - `TextRank` seems better suited for longer texts. This might be
   because `TextRank` needs larger graphs to do its magic.
 """
 
-import spacy
-import pytextrank               # noqa
 from vectorization.sparse_vectorization import build_tfidf_vectors
 from preprocessing.clean_text import TextCleaner
 from preprocessing.tokenize_text import TextTokenizer
 
-
-# Load spaCy model
-nlp = spacy.load('en_core_web_trf')
-nlp.add_pipe("textrank")
-
-
-text = """
-The curse of dimensionality refers to various phenomena that arise
-when analyzing and organizing data in high-dimensional spaces that do
-not occur in low-dimensional settings such as the three-dimensional
-physical space of everyday experience. The expression was coined by
-Richard E. Bellman when considering problems in dynamic programming.
-Dimensionally cursed phenomena occur in domains such as numerical
-analysis, sampling, combinatorics, machine learning, data mining and
-databases. The common theme of these problems is that when the
-dimensionality increases, the volume of the space increases so fast
-that the available data become sparse. In order to obtain a reliable
-result, the amount of data needed often grows exponentially with the
-dimensionality. Also, organizing and searching data often relies on
-detecting areas where objects form groups with similar properties; in
-high dimensional data, however, all objects appear to be sparse and
-dissimilar in many ways, which prevents common data organization
-strategies from being efficient.
-"""
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+from sumy.summarizers.lsa import LsaSummarizer
+from sumy.summarizers.text_rank import TextRankSummarizer
+from heapq import nlargest
 
 
-def extract_key_phrases(text, num_phrases=5):
-    """Extract key phrases from the given text."""
-    doc = nlp(text)
-    words = [(p.text, p.rank) for p in doc._.phrases]
-    words = sorted(words, key=lambda x: x[1], reverse=True)
-    return [word[0] for word in words[:num_phrases]]
+class TextSummarizer:
+    """Summarize text."""
 
+    def __init__(self, text):
+        """Initialize the TextSummarizer class."""
+        self.stemmer = Stemmer('english')
+        self.sumy_tokenizer = Tokenizer('english')
 
-def extract_sentences_and_tokens(text):
-    """Extract sentences and tokens from the given text."""
-    # Extract sentences
-    doc = nlp(text)
-    sentences = [sent.text for sent in doc.sents]
+        # Initialize sumy summarizers
+        self.sumy_summarizer_lsa = LsaSummarizer(self.stemmer)
+        self.sumy_summarizer_textrank = TextRankSummarizer(self.stemmer)
 
-    # Extract tokens
-    cleaner = TextCleaner()
-    tokenizer = TextTokenizer()
-    tokenized_sentences = [cleaner.clean_text(sent) for sent in sentences]
-    tokenized_sentences = [tokenizer.tokenize_text(sent)
-                           for sent in tokenized_sentences]
+        # Set stop words
+        self.sumy_summarizer_lsa.stop_words = get_stop_words('english')
+        self.sumy_summarizer_textrank.stop_words = get_stop_words('english')
 
-    return sentences, tokenized_sentences
+        # Parse text
+        self.parser = PlaintextParser.from_string(text, self.sumy_tokenizer)
+        self.sentences = [str(sent) for sent in self.parser.document.sentences]
 
+    def summarize_text_with_lsa(self, num_sentences=2):
+        """Summarize the given text with LSA."""
+        return [str(sent) for sent in self.sumy_summarizer_lsa(
+            self.parser.document, num_sentences)]
 
-def summarize_text_with_tfidf(text, num_sentences=2):
-    """Summarize the given text with TF-IDF."""
-    # Extract sentences and tokens
-    sentences, tokenized_sentences = extract_sentences_and_tokens(text)
+    def summarize_text_with_textrank(self, num_sentences=2):
+        """Summarize the given text with TextRank."""
+        return [str(sent) for sent in self.sumy_summarizer_textrank(
+            self.parser.document, num_sentences)]
 
-    # Build TF-IDF vectors
-    _, tfidf_vecs = build_tfidf_vectors(tokenized_sentences)
+    def summarize_text_with_tfidf(self, num_sentences=2):
+        """Summarize the given text with TF-IDF."""
+        # Extract tokens
+        cleaner = TextCleaner()
+        tokenizer = TextTokenizer()
+        tokenized_sentences = [cleaner.clean_text(sent)
+                               for sent in self.sentences]
+        tokenized_sentences = [tokenizer.tokenize_text(sent)
+                               for sent in tokenized_sentences]
 
-    # Sum tfidf scores for each sentence
-    sentence_scores = []
-    for i, doc_vec in enumerate(tfidf_vecs):
-        sentence_scores.append((i, sum([score for _, score in doc_vec])))
+        # Build TF-IDF vectors
+        _, tfidf_vecs = build_tfidf_vectors(tokenized_sentences)
 
-    # Sort sentences by TF-IDF score
-    sentence_scores = sorted(sentence_scores, key=lambda x: x[1],
-                             reverse=True)
+        # Sum tfidf scores for each sentence
+        def sum_tfidf_scores(doc_vec):
+            return sum([score for _, score in doc_vec])
 
-    # Return top sentences
-    return [sentences[i] for i, _ in sentence_scores[:num_sentences]]
+        sentence_scores = [(i, sum_tfidf_scores(doc_vec))
+                           for i, doc_vec in enumerate(tfidf_vecs)]
+
+        top_indices = nlargest(num_sentences, sentence_scores,
+                               key=lambda x: x[1])
+
+        # Return top sentences
+        return [self.sentences[i] for i, _ in top_indices]
