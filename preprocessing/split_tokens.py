26b7767842dc6e571e62acae5b6bbc379ac0aecb
mak-leibniz <makmiller@gmail.com>
2023-12-29 22:00:31 -0500

    Better incorporate spaCy Token attributes

--- preprocessing/split_tokens.py
+++ preprocessing/split_tokens.py
@@ -1,7 +1,6 @@
 """Split tokens using SymSpell and spaCy."""
 
 import spacy
-import re
 from symspellpy.symspellpy import SymSpell
 import pkg_resources
 
@@ -15,10 +14,7 @@ class SplitTokens:
 
     def __init__(self, path_dict=path):
         """Initialize the SplitTokens class."""
-        # Define alphanumeric regex pattern
-        self.pattern = r"^[a-zA-Z0-9]+$"
-
-        # Initialize SymSpell
+        # Set up SymSpell
         self.sym_spell = SymSpell()
         if self.sym_spell.load_dictionary(
                 path_dict, term_index=0, count_index=1):
@@ -32,8 +28,9 @@ class SplitTokens:
 
     def update_dictionary(self, text):
         """Use text to update SymSpell dictionary."""
-        doc = self.nlp.make_doc(text)
+        doc = self.nlp(text)
         for t in doc:
+            # Check whether t is alpha and longer than 1 char
             if (t.is_alpha and len(t.text) > 1):
                 self.sym_spell.create_dictionary_entry(t.text, 1)
 
@@ -46,26 +43,26 @@ class SplitTokens:
         lst_tokens = []
 
         for t in doc:
-            # Check if t is alphanumeric and not in the dictionary
-            if ((re.match(self.pattern, t.text) and
-                 (t.text not in self.sym_spell.words))):
+            if (t.is_alpha and
+               (t.ent_type == 0) and  # not an entity
+               (t.lemma_.lower() not in self.sym_spell.words)):
 
                 # Run SymSpell word segmentation
-                segmented_token = self.sym_spell.word_segmentation(
+                seg_token = self.sym_spell.word_segmentation(
                     t.text, max_edit_distance=max_edit_distance)
 
-                # Don't correct words and only accept segments if both
+                # Don't correct words and only accept segments if all
                 # segments are in the dictionary
-                if (segmented_token.corrected_string.replace(" ", "") == t.text
+                if (seg_token.corrected_string.replace(" ", "") == t.text
                     and all(part in self.sym_spell.words for
-                            part in segmented_token.corrected_string.split())):
+                            part in seg_token.corrected_string.split())):
 
                     # Save segmented token
                     lst_tokens.append(
-                        segmented_token.corrected_string + t.whitespace_)
+                        seg_token.corrected_string + t.whitespace_)
                 else:
-                    lst_tokens.append(t.text + t.whitespace_)
+                    lst_tokens.append(t.text_with_ws)
             else:
-                lst_tokens.append(t.text + t.whitespace_)
+                lst_tokens.append(t.text_with_ws)
 
         return "".join(lst_tokens)
