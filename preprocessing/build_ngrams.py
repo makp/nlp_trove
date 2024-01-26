2974a7ea598c2f2fd9f0eef63e4614043d61736f
mak-turing <makmiller@gmail.com>
2024-01-26 11:14:38 -0500

    Fix typo in docstring

--- preprocessing/build_ngrams.py
+++ preprocessing/build_ngrams.py
@@ -13,9 +13,9 @@ def train_phrase_model(tokenized_docs,
     - `min_cont`: Specifices the minimum number of times a bigram must
       appear. Accordingly, a higher value for this parameter means
       fewer bigrams.
-    - `threshold`: Speficies how strong the relationship between two
-      words must be to be considered a bigram. Accordingly, a higher
-      threshold means fewer bigrams.
+    - `threshold`: Specifies how strong the relationship between two
+      words must be to be considered a bigram. A higher threshold
+      means fewer bigrams.
     """
     phrase_model = Phraser(Phrases(tokenized_docs,
                                    min_count=min_count,
