5cc4c4a5ed8d5e49e6264c2acf4451414a7e644a
mak-turing <makmiller@gmail.com>
2024-06-09 13:34:09 -0400

    Add `api_key` as an argument and update submodule

--- data_acquisition/api/springer/download_springer_responses_as_json.py
+++ data_acquisition/api/springer/download_springer_responses_as_json.py
@@ -1,58 +1,50 @@
-import requests
 import json
 import time
-from data_processing.metadata import config_springer
 
+import requests
 
 BASE_URL = "https://api.springernature.com/metadata/json"
 WAIT_TIME = 1  # wait time between requests
 COUNT_PER_REQUEST = 50
 
-api_key = config_springer.API_KEY
 
-
-def get_number_results(query_str):
+def get_number_results(query_str, api_key):
     """Return the total number of results for a given query."""
     params = {
         "q": query_str,
-        "p": 1,   # Num of results per request
-        's': 1,   # Start index
-        "api_key": api_key
+        "p": 1,  # Num of results per request
+        "s": 1,  # Start index
+        "api_key": api_key,
     }
     response = requests.get(BASE_URL, params=params)
     data = response.json()
-    return int(data['result'][0]['total'])
+    return int(data["result"][0]["total"])
 
 
-def fetch_single_response(query_str, start):
+def fetch_single_response(query_str, start, api_key):
     """Make a single request to the API and return the response data."""
-    params = {
-        "q": query_str,
-        "p": COUNT_PER_REQUEST,
-        's': start,
-        "api_key": api_key
-    }
+    params = {"q": query_str, "p": COUNT_PER_REQUEST, "s": start, "api_key": api_key}
     response = requests.get(BASE_URL, params=params)
     return response.json()
 
 
-def fetch_all_responses(query_str):
+def fetch_all_responses(query_str, api_key):
     """Return all response data for a given query."""
-    total_records = get_number_results(query_str)
+    total_records = get_number_results(query_str, api_key)
     all_data = []
     for start in range(1, total_records + 1, COUNT_PER_REQUEST):
-        data = fetch_single_response(query_str, start)
+        data = fetch_single_response(query_str, start, api_key)
         all_data.append(data)
         time.sleep(WAIT_TIME)
     return all_data
 
 
-def download_responses(query_str, fname='out.json'):
+def download_responses(query_str, api_key, fname="out.json"):
     """Download all responses for a given query and save them as a
     JSON file.
     """
-    all_data = fetch_all_responses(query_str)
-    with open(fname, 'w') as f:
+    all_data = fetch_all_responses(query_str, api_key)
+    with open(fname, "w") as f:
         json.dump(all_data, f)
     print(f"Saved responses to {fname}")
     return fname
