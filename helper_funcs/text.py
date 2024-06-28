923ce6e8d65438096727cd62cc01e89bfa67e1aa
mak-turing <makmiller@gmail.com>
2024-06-28 11:15:54 -0400

    Add function calculate percentage text words that belong to a dictionary

--- helper_funcs/text.py
+++ helper_funcs/text.py
@@ -17,3 +17,14 @@ def search_for_long_words(text, min_length=15):
     # `{{` denotes a literal `{` inside an f-string.
     long_words = re.findall(rf"\b\w{{{min_length},}}\b", text)
     return long_words
+
+
+def get_percentage_in_dictionary(text, dictionary):
+    """Get percentage of words in a text that are in a dictionary."""
+    if not isinstance(text, str):
+        raise ValueError("Input must be a string.")
+    if not isinstance(dictionary, set):
+        raise ValueError("Dictionary must be a set.")
+    words = tokenize_with_regex(text.lower())
+    num_words_in_dict = sum(word in dictionary for word in words)
+    return num_words_in_dict / len(words) if words else 0
