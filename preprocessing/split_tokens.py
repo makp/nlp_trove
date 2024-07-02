57d2b13858d7308cd6e66b9a57805f54b2fbd118
mak-turing <makmiller@gmail.com>
2024-07-02 17:30:25 -0400

    Make minor formatting changes; Update submod commit

--- preprocessing/split_tokens.py
+++ preprocessing/split_tokens.py
@@ -1,8 +1,8 @@
 """Split tokens using SymSpell and spaCy."""
 
+import pkg_resources
 import spacy
 from symspellpy.symspellpy import SymSpell
-import pkg_resources
 
 
 class SplitTokens:
@@ -10,17 +10,17 @@ class SplitTokens:
 
     # Dictionary shipped with symspellpy
     path = pkg_resources.resource_filename(
-        "symspellpy", "frequency_dictionary_en_82_765.txt")
+        "symspellpy", "frequency_dictionary_en_82_765.txt"
+    )
 
     # Tell whether GPU is available
-    print("GPU available:", spacy.prefer_gpu())
+    print("GPU available:", spacy.prefer_gpu())  # type: ignore
 
     def __init__(self, path_dict=path):
         """Initialize the SplitTokens class."""
         # Set up SymSpell
         self.sym_spell = SymSpell()
-        if self.sym_spell.load_dictionary(
-                path_dict, term_index=0, count_index=1):
+        if self.sym_spell.load_dictionary(path_dict, term_index=0, count_index=1):
             print("Dictionary loaded.")
         else:
             print("Dictionary failed to load.")
@@ -33,18 +33,18 @@ class SplitTokens:
         doc = self.nlp.make_doc(text)
         for t in doc:
             # Check whether t is alpha and has a reasonable length
-            if (t.is_alpha and (20 > len(t.text) > 2)):
+            if t.is_alpha and (20 > len(t.text) > 2):
                 self.sym_spell.create_dictionary_entry(t.text, 1)
 
     def segment_token(self, token, cautious=True):
         """Segment a spaCy Token."""
         # Run word segmentation without correcting words
-        seg_token = self.sym_spell.word_segmentation(
-            token.text, max_edit_distance=0)
+        seg_token = self.sym_spell.word_segmentation(token.text, max_edit_distance=0)
 
         # Are all segs in the dictionary?
-        segs_in_dict = all(part in self.sym_spell.words for
-                           part in seg_token.corrected_string.split())
+        segs_in_dict = all(
+            part in self.sym_spell.words for part in seg_token.corrected_string.split()
+        )
 
         # Accept segmentation?
         accept_seg = segs_in_dict or (not cautious and len(token.text) >= 20)
@@ -63,11 +63,13 @@ class SplitTokens:
         lst_tokens = []
 
         for t in doc:
-            if (t.is_alpha and
-               (t.ent_type == 0) and  # not an entity
-               {t.lemma_.lower(), t.text.lower(), t.lemma_, t.text}.\
-               isdisjoint(set(self.sym_spell.words.keys()))):
-
+            if (
+                t.is_alpha
+                and (t.ent_type == 0)  # not an entity
+                and {t.lemma_.lower(), t.text.lower(), t.lemma_, t.text}.isdisjoint(
+                    set(self.sym_spell.words.keys())
+                )
+            ):
                 # Segment token
                 lst_tokens.append(self.segment_token(t, cautious=cautious))
             else:
