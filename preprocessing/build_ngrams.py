eb2000c5b3a38981b448f6bbf6b7aaa0deb0c0d8
mak-turing <makmiller@gmail.com>
2024-07-05 15:28:50 -0400

    Revise docstring; Change formatting; Update submodule commit

--- preprocessing/build_ngrams.py
+++ preprocessing/build_ngrams.py
@@ -1,24 +1,24 @@
 """Build n-grams from a list of tokenized documents."""
 
-from gensim.models.phrases import Phrases, Phraser, ENGLISH_CONNECTOR_WORDS
+from gensim.models.phrases import ENGLISH_CONNECTOR_WORDS, Phraser, Phrases
 
 
-def train_phrase_model(tokenized_docs,
-                       min_count=5,
-                       threshold=10):
+def train_phrase_model(tokenized_docs, min_count=5, threshold=10):
     """
     Train a phrase model on a list of tokenized docs.
 
     Note on the parameters:
-    - `min_cont`: Specifices the minimum number of times a bigram must
-      appear. Accordingly, a higher value for this parameter means
-      fewer bigrams.
-    - `threshold`: Specifies how strong the relationship between two
-      words must be to be considered a bigram. A higher threshold
-      means fewer bigrams.
+    - `min_cont`: Minimum number of times a word must appear to be considered
+      by the model. A higher value means fewer bigrams.
+    - `threshold`: Specifies how strong the relationship between two words must
+      be to be considered a bigram. A higher threshold means fewer bigrams.
     """
-    phrase_model = Phraser(Phrases(tokenized_docs,
-                                   min_count=min_count,
-                                   threshold=threshold,
-                                   connector_words=ENGLISH_CONNECTOR_WORDS))
+    phrase_model = Phraser(
+        Phrases(
+            tokenized_docs,
+            min_count=min_count,
+            threshold=threshold,
+            connector_words=ENGLISH_CONNECTOR_WORDS,
+        )
+    )
     return phrase_model
