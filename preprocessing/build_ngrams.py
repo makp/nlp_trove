985f98d07a5628c8afe93f909e88bf02ddd607df
mak-turing <makmiller@gmail.com>
2024-07-10 16:22:26 -0400

    Use `npmi` scoring for training bigram model

--- preprocessing/build_ngrams.py
+++ preprocessing/build_ngrams.py
@@ -1,9 +1,14 @@
 """Build n-grams from a list of tokenized documents."""
 
-from gensim.models.phrases import ENGLISH_CONNECTOR_WORDS, Phraser, Phrases
+from gensim.models.phrases import ENGLISH_CONNECTOR_WORDS, Phrases
 
 
-def train_phrase_model(tokenized_docs, min_count=5, threshold=10):
+def train_phrase_model(
+    tokenized_docs,
+    min_count=5,
+    threshold=0.7,
+    scoring="npmi",
+):
     """
     Train a phrase model on a list of tokenized docs.
 
@@ -11,14 +16,15 @@ def train_phrase_model(tokenized_docs, min_count=5, threshold=10):
     - `min_cont`: Minimum number of times a word must appear to be considered
       by the model. A higher value means fewer bigrams.
     - `threshold`: Specifies how strong the relationship between two words must
-      be to be considered a bigram. A higher threshold means fewer bigrams.
+      be to be considered a bigram. A higher threshold means fewer bigrams. For
+      npmi scoring, the threshold should be between -1 and +1.
     """
-    phrase_model = Phraser(
-        Phrases(
-            tokenized_docs,
-            min_count=min_count,
-            threshold=threshold,
-            connector_words=ENGLISH_CONNECTOR_WORDS,
-        )
+    phrase_model = Phrases(
+        tokenized_docs,
+        min_count=min_count,
+        threshold=threshold,
+        connector_words=ENGLISH_CONNECTOR_WORDS,
+        scoring=scoring,
     )
-    return phrase_model
+
+    return phrase_model.freeze()
