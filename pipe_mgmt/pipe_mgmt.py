0f5c1826c565f8ace37b65c860f41221b9dc9296
mak-turing <makmiller@gmail.com>
2025-01-07 17:51:44 -0500

    Revise funcs so that they apply to pipeline trees

--- pipe_mgmt/pipe_mgmt.py
+++ pipe_mgmt/pipe_mgmt.py
@@ -5,7 +5,9 @@ class PipeMgmt:
     Provides methods for creating, searching and analyzing pipelines.
 
     The term "pipeline tree" refers a list of dictionaries object, where each
-    dictionary represents a pipeline with its attributes and children.
+    dictionary represents a pipeline with its attributes and children. Note
+    that the pipeline children has the same structure as a pipeline tree (i.e.,
+    `list[dict]`).
     """
 
     pipe_key_types = {
@@ -26,37 +28,37 @@ class PipeMgmt:
             if not is_valid:
                 raise ValueError("Invalid pipeline tree structure")
 
-    def _filter_pipe(self, pipe: dict, attrib="name") -> list[list]:
-        """Filter a pipeline based on an attribute key."""
-        neted_list = [pipe[attrib]]
-        if pipe.get("children"):
-            for child in pipe["children"]:
-                neted_list.append(self._filter_pipe(child, attrib))
-        return neted_list
+    def _filter_pipetree(self, pipe_tree: list[dict], attrib="name"):
+        """
+        Filter pipeline tree based on an attribute key.
 
-    def _append_head_to_sublists(self, head: str, sublist: list[list]) -> list[list]:
-        return [[head] + sublist for sublist in sublist]
+        The dictionary representing a pipeline with children has the following
+        structure: `{kwargs, children: list}`. The output of this function will
+        return `[[val_parent, [[val_child1, [[val_grandchild1, ...]], ...]]]]`.
+        """
+        out_lst = []
+        for pipe in pipe_tree:
+            filtered_pipe = [pipe[attrib]]
+            if pipe.get("children"):
+                filtered_pipe.append(self._filter_pipetree(pipe["children"], attrib))
+            out_lst.append(filtered_pipe)
+        return out_lst
 
-    def _flatten_nested_list(self, nested_list: list[list]):
-        """List all paths in a nested list."""
-        if len(nested_list) == 1:
-            return [nested_list]
-        elif len(nested_list) > 1:
-            head, *tail = nested_list
-            path = []
-            for member in tail:
-                path.extend(
-                    self._append_head_to_sublists(
-                        head,  # type: ignore
-                        self._flatten_nested_list(member),  # type: ignore
-                    )
-                )
-            return path
+    def _flatten_filtered_pipetree(self, nested_list: list[list]):
+        """Flatten a nested list produced by `_filter_pipetree`."""
+        paths = []
+        for node in nested_list:
+            if len(node) == 1:  # Check if it's a terminal node
+                paths.append(node)
+            else:  # Non-terminal nodes have format `[val, [[val, ...]]]`
+                for member in self._flatten_filtered_pipetree(node[1]):
+                    paths.append([node[0]] + member)
+        return paths
 
-    def list_paths(self, pipe: dict) -> list[list]:
-        """List all paths within a pipeline from root to terminal nodes."""
-        nested_list = self._filter_pipe(pipe)
-        return self._flatten_nested_list(nested_list) or []
+    def list_paths(self, pipe_tree, attrib="name"):
+        """List all pipeline paths."""
+        filtered_tree = self._filter_pipetree(pipe_tree, attrib)
+        return self._flatten_filtered_pipetree(filtered_tree)
 
     def validate_pipe(self, pipe: dict) -> bool:
         """
