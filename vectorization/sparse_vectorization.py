f679078a82baaa22309ea0cf344983fba2085a31
mak-turing <makmiller@gmail.com>
2025-01-21 15:54:11 -0500

    feat!: group sparse vectorization functions into a class

--- vectorization/sparse_vectorization.py
+++ vectorization/sparse_vectorization.py
@@ -1,59 +1,42 @@
-"""Transform text into sparse vectors."""
+"""Transform text into sparse vectors using Gensim."""
 
 from gensim.corpora import Dictionary
 from gensim.models import TfidfModel
 
 
-def map_tokens_to_integer_ids(tokenized_docs,
-                              no_below=1,
-                              no_above=1,
-                              keep_n=None):
-    """
-    Build a dictionary from tokenized documents.
+class SparseVec:
+    def __init__(self, docs):
+        self.docs = docs
+        self.tk_id_map = Dictionary(self.docs)
 
-    When building a dictionary, filter out frequent and infrequent
-    tokens. The keyword argument `no_below` is an absolute number and
-    `no_above` is a fraction of the total corpus size. Keep `keep_n`
-    most frequent tokens after the above filtering. Keep all if `None`.
-    """
-    # Create a dictionary
-    id2word = Dictionary(tokenized_docs)
+    def filter_tokens(self, no_below_n=1, no_above_f=1, keep_n=None):
+        """
+        Filter tokens and update the dictionary.
 
-    # Filter out certain tokens
-    id2word.filter_extremes(no_below=no_below, no_above=no_above, keep_n=keep_n)  # type: ignore
+        The kwarg `no_below_n` is the number of documents and `no_above_f` is a
+        fraction of the total docs. Keep `keep_n` most frequent tokens after
+        the above filtering. Keep all if `None`.
+        """
+        self.tk_id_map.filter_extremes(
+            no_below=no_below_n,
+            no_above=no_above_f,
+            keep_n=keep_n,  # type: ignore
+        )
 
-    return id2word
+    def build_bow_vectors(self):
+        """
+        Build bag-of-words (BoW) vectors from tokenized documents.
 
+        BoW vectors are tuples of token IDs and their counts.
+        """
+        return [self.tk_id_map.doc2bow(doc) for doc in self.docs]
 
-def build_bow_vectors(tokenized_docs,
-                      no_below=1,
-                      no_above=1,
-                      keep_n=None):
-    """Build bag-of-words vectors from tokenized documents."""
-    id2word = map_tokens_to_integer_ids(tokenized_docs,
-                                        no_below=no_below,
-                                        no_above=no_above,
-                                        keep_n=keep_n)
+    def build_tfidf_vectors(self):
+        """
+        Build TF-IDF vectors from tokenized documents.
 
-    # Convert tokenized documents into bow vectors
-    bow_corpus = [id2word.doc2bow(doc) for doc in tokenized_docs]
-
-    # Return dictionary and corpus as bow vecs
-    return id2word, bow_corpus
-
-
-def build_tfidf_vectors(tokenized_docs,
-                        no_below=1,
-                        no_above=1,
-                        keep_n=None):
-    """Build TF-IDF vectors from tokenized documents."""
-    id2word, bow_corpus = build_bow_vectors(tokenized_docs,
-                                            no_below=no_below,
-                                            no_above=no_above,
-                                            keep_n=keep_n)
-
-    # Fit the TF-IDF model
-    tfidf = TfidfModel(dictionary=id2word)
-
-    # Return dictionary and corpus as TF-IDF vecs
-    return id2word, tfidf[bow_corpus]
+        The returned vectors are tuples of token IDs and TF-IDF weights.
+        """
+        bow_vecs = self.build_bow_vectors()
+        tfidf = TfidfModel(bow_vecs)
+        return [tfidf[vec] for vec in bow_vecs]
