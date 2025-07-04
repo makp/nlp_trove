eba3455a08fa198c1106ed0f8ba9ff8601d47c86
mak-turing <makmiller@gmail.com>
2025-07-04 10:04:40 -0400

    feat(preprocessing): update class for XML inspection

--- preprocessing/process_xml.py
+++ preprocessing/process_xml.py
@@ -31,16 +31,29 @@ class InspectXML:
         }
         return root_lengths
 
+    def _return_shared_root_children(self):
+        """
+        Return the children of the root elements that are shared across all XML files.
+        """
+        shared_children = set()
+        for filename in self.file_paths:
+            root = self._return_root(filename)
+            root_children = {child.tag for child in root}
+            if not shared_children:
+                shared_children = root_children
+            else:
+                shared_children.intersection_update(root_children)
+        return shared_children
+
     def check_root(self):
         """
         Check the root elements of the XML files.
         """
-        root_tags = self._return_root_tags()
-        root_lengths = self._return_root_lengths()
-
-        print(f"Root tags: {root_tags}")
+        print(f"Root tags: {self._return_root_tags()}")
         print("-" * 50)
-        print(f"Root lengths: {root_lengths}")
+        print(f"Root lengths: {self._return_root_lengths()}")
+        print("-" * 50)
+        print(f"Shared root children: {self._return_shared_root_children()}")
 
     def _return_root_namespaces(self):
         """
@@ -52,6 +65,17 @@ class InspectXML:
             all_namespaces.update(set(root.nsmap.items()))
         return all_namespaces
 
+    def _return_namespaces_from_file(self, filename: str):
+        """
+        Return the namespaces of the root element of the XML file with the given
+        filename.
+        """
+        file_namespaces = set()
+        root = self._return_root(filename)
+        for el in root.iter("{*}*"):
+            file_namespaces.update(set(el.nsmap.items()))
+        return file_namespaces
+
     def _return_all_namespaces(self):
         """
         Search for all namespaces in the XML files and return a set of their
@@ -59,9 +83,8 @@ class InspectXML:
         """
         all_namespaces = set()
         for filename in self.file_paths:
-            root = self._return_root(filename)
-            for el in root.iter("{*}*"):
-                all_namespaces.update(set(el.nsmap.items()))
+            file_namespaces = self._return_namespaces_from_file(filename)
+            all_namespaces.update(file_namespaces)
         return all_namespaces
 
     def _return_elements_sans_namespace(self):
@@ -75,19 +98,30 @@ class InspectXML:
             elements_sans_namespace.update(tags_sans_namespace)
         return elements_sans_namespace
 
+    def _return_shared_namespaces(self):
+        """
+        Return namespaces shared across all XML files.
+        """
+        shared_namespaces = set()
+        for filename in self.file_paths:
+            namespaces = self._return_namespaces_from_file(filename)
+            if not shared_namespaces:
+                shared_namespaces = namespaces
+            else:
+                shared_namespaces.intersection_update(namespaces)
+        return shared_namespaces
+
     def check_namespaces(self):
         """
-        Check if the XML files have namespaces.
+        Print information about namespaces in the XML files.
         """
-        root_namespaces = self._return_root_namespaces()
-        all_namespaces = self._return_all_namespaces()
-        elements_sans_namespace = self._return_elements_sans_namespace()
-
-        print(f"Root namespaces: {root_namespaces}")
+        print(f"Root namespaces: {self._return_root_namespaces()}")
         print("-" * 50)
-        print(f"All namespaces: {all_namespaces}")
+        print(f"All namespaces: {self._return_all_namespaces()}")
         print("-" * 50)
-        print(f"Tags without namespace: {elements_sans_namespace}")
+        print(f"Shared namespaces: {self._return_shared_namespaces()}")
+        print("-" * 50)
+        print(f"Tags without namespace: {self._return_elements_sans_namespace()}")
 
 
 # class ProcessXML:
